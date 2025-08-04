d4fa9dccc926c2e37e29b10ca61565aedd3be75f
NoveIX <stefy.ferri2000@gmail.com>
2025-08-04 23:05:23 +0200

    Update path tools

--- tools/client/win/Setup.cmd
+++ tools/client/win/Setup.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0\..\src\app\Main.ps1" -Setup
+powershell -ExecutionPolicy Bypass -File "%~dp0\..\..\src\app\Main.ps1" -Setup
--- tools/client/win/Update.cmd
+++ tools/client/win/Update.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0\..\src\app\Main.ps1" -Update
+powershell -ExecutionPolicy Bypass -File "%~dp0\..\..\src\app\Main.ps1" -Update
