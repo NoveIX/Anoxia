14121ecb75c381c7e1fb4f7d9eecea6a74fbc908
NoveIX <stefy.ferri2000@gmail.com>
2025-08-05 18:08:24 +0200

    restore update in a file

--- tools/server/run.sh
+++ tools/server/run.sh
@@ -4,8 +4,34 @@
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 # pass them to this script directly
 
-# Check update
-./update.sh
+#!/bin/bash
+
+# Locate script path
+scriptDir=$(dirname "$(realpath "$0")")
+serverDir=$scriptDir
+cd $serverDir
+
+# Define parent path
+toolsDir=$(realpath "$serverDir/..")
+modpackDir=$(realpath "$toolsDir/..")
+
+# Define key, file and path
+keyDir="$toolsDir/src/key"
+sshKey_PrivatePath="$keyDir/anoxia_github_readonly"
+sshKey_PublicPath="$keyDir/anoxia_github_readonly.pub"
+
+# Define forge and minecraft version
+mcVer=1.20.1
+forgeVer=47.4.0
+
+# Change directory modpack dir
+cd $modpackDir
+auth="$sshKey_PrivatePath"
+
+# Check update and start server
+echo -e "\n# ===================== Git log message ===================== #\n"
+GIT_SSH_COMMAND="ssh -i $auth" git pull
+echo -e "\n# =========================================================== #\n"
 
 # Run Server
 tmux new-session -d -s "Anoxia" "export VAR=value; java @user_jvm_args.txt @libraries/net/minecraftforge/forge/$mcVer-$forgeVer/unix_args.txt \"$@\""
