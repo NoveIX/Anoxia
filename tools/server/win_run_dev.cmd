f74640b3fe65a276b6f236cf6ffa5a129db2886a
NoveIX <stefy.ferri2000@gmail.com>
2025-08-05 14:31:34 +0200

    Change directory exacly with client folder

--- tools/server/win_run_dev.cmd
+++ tools/server/win_run_dev.cmd
@@ -1,7 +1,7 @@
 @echo off
 REM Nome dello script (nome + estensione)
 set "script_name=%~nx0"
-powershell -ExecutionPolicy Bypass -File "%~dp0\..\..\app\Main.ps1" -Server -InvokeCMD %script_name%
+powershell -ExecutionPolicy Bypass -File "%~dp0\..\src\app\Main.ps1" -Server -InvokeCMD %script_name%
 
 REM Versioni Forge e Minecraft
 SET "mcVer=1.20.1"
