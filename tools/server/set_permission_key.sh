2c0e4ec4e457e86ab9931322a5b510e66a178a64
NoveIX <stefy.ferri2000@gmail.com>
2025-08-05 17:27:39 +0200

    Create set_permission_key.sh

---
+++ tools/server/set_permission_key.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Locate script path
+scriptDir=$(dirname "$(realpath "$0")")
+serverDir=$scriptDir
+cd $serverDir
+
+# Define parent path
+toolsDir=$(realpath "$serverDir/..")
+modpackDir=$(realpath "$toolsDir/..")
+
+# Define key, file and path
+keyDir="$toolsDir/src/key"
+sshKey_PrivatePath="$keyDir/anoxia_github_readonly"
+sshKey_PublicPath="$keyDir/anoxia_github_readonly.pub"
+
+chmod 700 $keyDir
+chmod 600 $sshKey_PrivatePath
+chmod 644 $sshKey_PublicPath
