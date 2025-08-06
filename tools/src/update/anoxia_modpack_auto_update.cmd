0f6bb0ed0d079bf77140c1d25c9688de4c9cae1c
NoveIX <stefy.ferri2000@gmail.com>
2025-08-06 17:38:44 +0200

    Insert open loader

--- tools/src/update/anoxia_modpack_auto_update.cmd
+++ tools/src/update/anoxia_modpack_auto_update.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "targetFile=%TEMP%\NoveLib\MoonBase2_modpack_path.txt"
+set "targetFile=%TEMP%\NoveLib\anoxia_modpack_path.txt"
 
 REM Check existence of txt file in temp
 if not exist "%targetFile%" (
