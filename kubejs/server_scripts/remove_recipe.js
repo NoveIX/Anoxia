738df6fbff33a9c29b15a85ca1ca2d4c9be740cb
NoveIX <stefy.ferri2000@gmail.com>
2025-09-02 17:51:34 +0200

    Update remove_recipe.js

--- kubejs/server_scripts/remove_recipe.js
+++ kubejs/server_scripts/remove_recipe.js
@@ -10,8 +10,8 @@ ServerEvents.recipes((event) => {
     });
 
     global.items.GAME_FullRm.forEach((item) => {
-        //event.remove({ output: item });
-        event.remove({ input: item });
+        event.remove({ output: item });
+        //event.remove({ input: item });
     });
 
     //Fluid
