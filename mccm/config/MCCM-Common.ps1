9e778ae354899698a1ae4e1449d39d7e4b40d7cc
NoveIX <stefy.ferri2000@gmail.com>
2025-10-04 18:58:56 +0200

    Update feature

---
+++ mccm/config/MCCM-Common.ps1
@@ -0,0 +1,21 @@
+    # Terminal Title
+    [string]$Script:PSTitle = "Project Anoxia Lunar Ruins"
+
+    # Repository info
+    [string]$Script:RepositoryUrl = "https://github.com/NoveIX/Anoxia.git"
+    [string]$Script:RepositoryBranch = "1.20"
+
+    # SSH key Url id
+    [string]$Script:PrivateKeyUrlID = "16nw7QJfa-BxG74haagu6x4UO_gFlPv6a"
+    [string]$Script:PublicKeyUrlID = "1Q2nL9ulE3s12coUepO4HPcJ4ULsbPCFU"
+
+    # SSH key file name
+    [string]$Script:PrivateKeyName = "anoxia_github_readonly"
+    [string]$Script:PublicKeyName = "anoxia_github_readonly.pub"
+
+    # Auto update
+    [string]$Script:AutoUpdateTXT = "anoxia_modpack_path.txt"
+    [string]$Script:AutoUpdateCMD = "anoxia_modpack_auto_update.cmd"
+
+    # Cloud Direct Url
+    [string]$Script:CloudDirectUrl = "https://drive.google.com/uc?export=download&id="
---
+++ mccm/source/library/PSModpack/PSModpack.psm1
@@ -0,0 +1,81 @@
+#region UI Git
+function Invoke-UIGitTop { Write-Host "`n# ===================== Git log message ===================== #`n" }
+function Invoke-UIGitBot { Write-Host "`n# =========================================================== #`n" }
+#endregion
+
+# ================================================================================================ #
+
+#region Selection
+function Confirm-Selection {
+    [CmdletBinding()]
+    param (
+        [string]$Message,
+        [string]$Answer = $null
+    )
+
+    Write-Host "$Message (Y/n): " -NoNewline
+    if ($null -ne $Answer) {
+        $Answer = Read-Host
+    }
+    else {
+        Write-Host "Y"
+    }
+
+    if ($Answer -eq "Y" -or [string]::IsNullOrWhiteSpace($Answer)) {
+        return $true
+    }
+    else {
+        return $false
+    }
+}
+#endregion
+
+# ================================================================================================ #
+
+#region git pull
+function Invoke-GitPull {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Location
+    )
+
+    [string]$origin = git -C $Location config --get remote.origin.url
+    if ($origin -like "git@*") {
+        $env:GIT_SSH_COMMAND = "ssh -i `"$PrivateKeyPath`""
+        git -C $Location pull
+    }
+    else {
+        git -C $Location pull
+    }
+}
+#endregion
+
+# ================================================================================================ #
+
+#region git clone
+function Invoke-GitClone {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$RepoUrl,
+        [string]$RepoBranch,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Location,
+
+        [Parameter(Mandatory = $true)]
+        [string]$PrivateKeyPath
+    )
+
+
+    if ($RepoUrl -like "git@*") {
+        $env:GIT_SSH_COMMAND = "ssh -i `"$PrivateKeyPath`""
+        if ($RepoBranch) { git clone -b $RepoBranch --depth 1 --single-branch $RepoUrl $Location }
+        else { git clone --depth 1 --single-branch $RepoUrl $Location }
+    }
+    else {
+        if ($RepoBranch) { git clone -b $RepoBranch --depth 1 --single-branch $RepoUrl $Location }
+        else { git clone --depth 1 --single-branch $RepoUrl $Location }
+    }
+}
+#endregion
