f01b509197ef057b6ba47392e8980d833ad8d957
NoveIX <stefy.ferri2000@gmail.com>
2026-02-05 00:58:44 +0100

    Update Global

--- kubejs/startup_scripts/item_modification.js
+++ kubejs/startup_scripts/item_modification.js
@@ -1,14 +1,14 @@
 ItemEvents.modification((event) => {
-    event.modify("anoxia:tiny_coal", (item) => (item.burnTime = 200));
-    event.modify("anoxia:tiny_charcoal", (item) => (item.burnTime = 200));
-    event.modify("thermal:sawdust", (item) => (item.burnTime = 100));
-    event.modify("mekanism:sawdust", (item) => (item.burnTime = 100));
+  event.modify('anoxia:tiny_coal', (item) => (item.burnTime = 200));
+  event.modify('anoxia:tiny_charcoal', (item) => (item.burnTime = 200));
+  event.modify('thermal:sawdust', (item) => (item.burnTime = 100));
+  event.modify('mekanism:sawdust', (item) => (item.burnTime = 100));
 
-    //Vanilla Max Stack Size
-    event.modify(Item.of("minecraft:potion"), (data) => { data.maxStackSize = 64; });
-    event.modify(Item.of("minecraft:splash_potion"), (data) => { data.maxStackSize = 64; });
-    event.modify(Item.of("minecraft:lingering_potion"), (data) => { data.maxStackSize = 64; });
-    event.modify(Item.of("minecraft:ender_pearl"), (data) => { data.maxStackSize = 64; });
-    event.modify(Item.of("minecraft:egg"), (data) => { data.maxStackSize = 64; });
-    event.modify(Item.of("minecraft:bucket"), (data) => { data.maxStackSize = 64; });
+  //Vanilla Max Stack Size
+  event.modify(Item.of('minecraft:potion'), (data) => (data.maxStackSize = 64));
+  event.modify(Item.of('minecraft:splash_potion'), (data) => (data.maxStackSize = 64));
+  event.modify(Item.of('minecraft:lingering_potion'), (data) => (data.maxStackSize = 64));
+  event.modify(Item.of('minecraft:ender_pearl'), (data) => (data.maxStackSize = 64));
+  event.modify(Item.of('minecraft:egg'), (data) => (data.maxStackSize = 64));
+  event.modify(Item.of('minecraft:bucket'), (data) => (data.maxStackSize = 64));
 });
