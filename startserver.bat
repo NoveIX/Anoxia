d2ee2a719795db3b8cff87305b618a40cce1bba7
NoveIX <stefy.ferri2000@gmail.com>
2026-03-11 23:32:15 +0100

    add server script

---
+++ startserver.bat
@@ -0,0 +1,99 @@
+@echo off
+title Anoxia Server
+
+:: To use a specific Java runtime, define the JAVA variable below with the full path to java.exe.
+:: set ANOXIA_JAVA=C:\Program Files\Java\jdk-17\bin\java.exe
+
+:: To disable automatic restarts, set the AUTO_RESTART variable to false.
+:: set ANOXIA_RESTART=false
+
+:: To install the pack without starting the server, set the INSTALL_ONLY variable to true.
+:: set ANOXIA_INSTALL_ONLY=true
+
+set FORGE_VERSION=47.4.10
+set INSTALLER="forge-1.20.1-%FORGE_VERSION%-installer.jar"
+set FORGE_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-%FORGE_VERSION%/forge-1.20.1-%FORGE_VERSION%-installer.jar"
+
+:JAVA
+:: Set default Java
+if not defined ANOXIA_JAVA (
+    set ANOXIA_JAVA=java
+)
+
+:: Check Java is installed
+"%ANOXIA_JAVA%" -version 1>nul 2>nul || (
+   echo Minecraft 1.20.1 requires Java 17 - Java not found
+   pause
+   exit /b 1
+)
+
+:FORGE
+:: Install or download Minecraft Forge
+setlocal enabledelayedexpansion
+cd /D "%~dp0"
+if not exist "libraries" (
+    echo Forge not installed, installing now.
+    if not exist %INSTALLER% (
+        echo No Forge installer found, downloading now.
+
+            :: try curl
+            where /q curl
+            if %errorlevel%==0 (
+                echo "DEBUG: (curl) Downloading from %FORGE_URL%"
+                curl.exe -# -o "%INSTALLER%" -L "%FORGE_URL%"
+            ) else (
+
+                :: try bitsadmin
+                where /q bitsadmin
+                if %errorlevel%==0 (
+                    echo "DEBUG: (bitsadmin) Downloading from %FORGE_URL%"
+                    bitsadmin.exe /rawreturn /nowrap /transfer forgeinstaller /download /priority FOREGROUND %FORGE_URL% %INSTALLER%
+                ) else (
+                    echo "Neither powershell or curl or bitsadmin were found on your system. Please install one and try again"
+                    pause
+                    exit /b 1
+                )
+            )
+        )
+
+    echo Running Forge installer.
+    "%ANOXIA_JAVA%" -jar %INSTALLER% -installServer
+)
+
+:: Create default server properties
+if not exist "server.properties" (
+    (
+        echo allow-flight=true
+        echo difficulty=hard
+        echo enable-command-block=true
+        echo motd=§3Project§r §2Anoxia§r §7Lunar§r §4Ruins§r
+        echo max-tick-time=180000
+    )> "server.properties"
+)
+
+:: End install only
+if "%ANOXIA_INSTALL_ONLY%" == "true" (
+    echo INSTALL_ONLY: complete
+    goto:EOF
+)
+
+:: Check Java version
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('"%ANOXIA_JAVA%" -fullversion 2^>^&1') do set "jver=%%j"
+if not %jver% geq 17  (
+    echo Minecraft 1.20.1 requires Java 17 - found Java %jver%
+    pause
+    exit /b 1
+)
+
+:START
+:: Server Start or restart if crash handle on
+"%ANOXIA_JAVA%" @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-%FORGE_VERSION%/win_args.txt nogui
+
+:: Restart Server
+if "%ANOXIA_RESTART%" == "false" (
+    goto:EOF
+)
+
+echo Restarting automatically in 10 seconds (press Ctrl + C to cancel)
+timeout /t 10 /nobreak > NUL
+goto:START
---
+++ startserver.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+set -eu
+
+# To use a specific Java runtime, define the JAVA variable below with the full path to java.exe.
+# ANOXIA_JAVA=/usr/lib/jvm/java-17-openjdk-arm64/bin/java
+
+# To disable automatic restarts, set the AUTO_RESTART variable to false.
+# ANOXIA_RESTART=false
+
+# To install the pack without starting the server, set the INSTALL_ONLY variable to true.
+# ANOXIA_INSTALL_ONLY=true
+
+FORGE_VERSION=47.4.10
+INSTALLER="forge-1.20.1-$FORGE_VERSION-installer.jar"
+FORGE_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-$FORGE_VERSION/forge-1.20.1-$FORGE_VERSION-installer.jar"
+
+pause() {
+    printf "%s\n" "Press enter to continue..."
+    read ans
+}
+
+# Check Java is installed
+if ! command -v "${ANOXIA_JAVA:-java}" >/dev/null 2>&1; then
+    echo "Minecraft 1.20.1 requires Java 17 - Java not found"
+    pause
+    exit 1
+fi
+
+# Install or download Minecraft Forge
+cd "$(dirname "$0")"
+if [[ ! -d libraries ]]; then
+    echo "Forge not installed, installing now."
+    if [[ ! -f "$INSTALLER" ]]; then
+        echo "No Forge installer found, downloading now."
+        # try wget
+        if command -v wget >/dev/null 2>&1; then
+            echo "DEBUG: (wget) Downloading $FORGE_URL"
+            wget -O "$INSTALLER" "$FORGE_URL"
+        else
+            # try curl
+            if command -v curl >/dev/null 2>&1; then
+                echo "DEBUG: (curl) Downloading $FORGE_URL"
+                curl -o "$INSTALLER" -L "$FORGE_URL"
+            else
+                echo "Neither wget or curl were found on your system. Please install one and try again"
+                pause
+                exit 1
+            fi
+        fi
+    fi
+
+    echo "Running Forge installer."
+    "${ANOXIA_JAVA:-java}" -jar "$INSTALLER" -installServer
+fi
+
+# Create default server properties
+if [[ ! -f server.properties ]]; then
+    cat <<EOF > server.properties
+allow-flight=true
+difficulty=hard
+enable-command-block=true
+motd=§3Project§r §2Anoxia§r §7Lunar§r §4Ruins§r
+max-tick-time=180000
+EOF
+fi
+
+# End install only
+if [[ "${ANOXIA_INSTALL_ONLY:-false}" = "true" ]]; then
+    echo "INSTALL_ONLY: complete"
+    exit 0
+fi
+
+# Check Java version
+JAVA_VERSION=$("${ANOXIA_JAVA:-java}" -fullversion 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d'.' -f1)
+if [[ ! "$JAVA_VERSION" -ge 17 ]]; then
+    echo "Minecraft 1.20.1 requires Java 17 - found Java $JAVA_VERSION"
+    pause
+    exit 1
+fi
+
+# Server Start or restart if crash handle on
+while true
+do
+    "${ANOXIA_JAVA:-java}" @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-$FORGE_VERSION/unix_args.txt nogui || EXIT_CODE=$?
+
+    if [[ "${ANOXIA_RESTART:-true}" = "false" ]]; then
+        exit 0
+    fi
+
+    echo "Restarting automatically in 10 seconds (press Ctrl + C to cancel)"
+    sleep 10
+done
