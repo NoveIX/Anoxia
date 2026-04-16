50fbb23354a4fd614bb1f04b1c2d033c4c716c68
NoveIX <stefy.ferri2000@gmail.com>
2026-04-17 00:01:09 +0200

    Update hyperbox.js

--- kubejs/server_scripts/mods/hyperbox.js
+++ kubejs/server_scripts/mods/hyperbox.js
@@ -1,4 +1,4 @@
 ServerEvents.recipes((event) => {
   event.remove({ output: 'hyperbox:hyperbox' });
-  event.shaped('hyperbox:hyperbox', ['ABA', 'CDC', 'AEA'], { A: '#forge:storage_blocks/diamond', B: 'uppers:upper', C: '#forge:barrels/wooden', D: 'entangled:block', E: 'minecraft:hopper' });
+  event.shaped('hyperbox:hyperbox', ['ABA', 'CDC', 'AEA'], { A: '#forge:storage_blocks/diamond', B: 'uppers:upper', C: '#forge:barrels/wooden', D: 'industrialforegoing:machine_frame_supreme', E: 'minecraft:hopper' });
 });
