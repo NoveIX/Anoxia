e64612986ec8a39baad781585a37fcdacf0d3598
NoveIX <stefy.ferri2000@gmail.com>
2026-05-05 21:57:51 +0200

    formatting

--- mcsu/Client update.cmd
+++ mcsu/Client update.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0\source\app\Main.ps1" -Update
+powershell -ExecutionPolicy Bypass -File "%~dp0source\app\Main.ps1" -Update
 if errorlevel 1 (pause & exit /b 1)
--- mcsu/client/Menu.cmd
+++ mcsu/client/Menu.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0\..\source\app\Main.ps1" -Menu
+powershell -ExecutionPolicy Bypass -File "%~dp0..\source\app\Main.ps1" -Menu
 if errorlevel 1 (pause & exit /b 1)
